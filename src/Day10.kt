fec7a25431c07f917b813d57ce868e9cd194fd9e
CHervaudBetclic <c.hervaud@betclicgroup.com>
2025-12-10 10:11:05 +0100

    Add day 10 part 2

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,9 +1,9 @@
 typealias Vector = List<Int>
 
 data class Machine(
-  val goal: Vector,
+  val lightIndicator: Vector,
   val buttons: List<Vector>,
-  val joltage: List<Int>,
+  val joltage: Vector,
 )
 
 fun main() {
@@ -33,7 +33,7 @@ fun main() {
       .let {
         val goal = it.first().trim('[', ']').toVectorFromHashtags()
         Machine(
-          goal = goal,
+          lightIndicator = goal,
           buttons = it.drop(1).dropLast(1).map { b -> b.trim('(', ')').toInts().toVectorFromInts(goal.size) },
           joltage = it.last().trim('{', '}').toInts(),
         )
@@ -43,7 +43,9 @@ fun main() {
 
   operator fun Vector.plus(other: Vector) = zip(other) { a, b -> a + b }
 
-  fun List<Vector>.sum() = reduce { acc, v -> acc + v }.map { it % 2 }
+  fun List<Vector>.sumWithModulo() = reduce { acc, v -> acc + v }.map { it % 2 }
+
+  fun List<Vector>.sum() = reduce { acc, v -> acc + v }
 
   fun <T> List<T>.combinations(n: Int): List<List<T>> {
     if (n == 0) return listOf(emptyList())
@@ -64,11 +66,37 @@ fun main() {
     return result
   }
 
-  fun Machine.getLowestPresses(): Int {
+  fun <T> List<T>.combinationsWithRepetition(n: Int): List<List<T>> {
+    fun helper(start: Int, k: Int): List<List<T>> {
+      if (k == 0) return listOf(emptyList())
+      val result = mutableListOf<List<T>>()
+
+      for (i in start until size) {
+        for (tail in helper(i, k - 1)) {
+          result += listOf(get(i)) + tail
+        }
+      }
+
+      return result
+    }
+    return helper(0, n)
+  }
+
+  fun Machine.getLowestPressesForLightIndicator(): Int {
     for (i in 1..1000) {
       val combinations = buttons.combinations(i)
       combinations.forEach { combination ->
-        if (combination.sum() == goal) return i
+        if (combination.sumWithModulo() == lightIndicator) return i
+      }
+    }
+    throw Exception("Reach maximum try without finding solution")
+  }
+
+  fun Machine.getLowestPressesForJoltage(): Int {
+    for (i in 1..1000) {
+      val combinations = buttons.combinationsWithRepetition(i)
+      combinations.forEach { combination ->
+        if (combination.sum() == joltage) return i
       }
     }
     throw Exception("Reach maximum try without finding solution")
@@ -78,11 +106,17 @@ fun main() {
     val machines = input.parse()
 
     return machines.sumOf { machine ->
-      machine.getLowestPresses()
+      machine.getLowestPressesForLightIndicator()
     }
   }
 
-  fun part2(input: List<String>) = input.size
+  fun part2(input: List<String>): Int {
+    val machines = input.parse()
+
+    return machines.sumOf { machine ->
+      machine.getLowestPressesForJoltage()
+    }
+  }
 
 //     Test if implementation meets criteria from the description, like:
 //    check(part1(listOf("test_input")) == 1)
@@ -90,7 +124,7 @@ fun main() {
 //     Or read a large test input from the `src/Day10_test.txt` file:
   val testInput = readInput("Day10_test")
   check(part1(testInput) == 7)
-//  check(part2(testInput) == 24)
+  check(part2(testInput) == 33)
 
 //     Read the input from the `src/Day10.txt` file.
   val input = readInput("Day10")
